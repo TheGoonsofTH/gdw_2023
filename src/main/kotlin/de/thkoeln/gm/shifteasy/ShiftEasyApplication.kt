8eafabe5d895c65b5ecad6f13d7c25c8490b3539
marc <marc@cremer.dev>
2023-10-30 10:41:08 +0100

    init

---
+++ src/main/kotlin/de/thkoeln/gm/shifteasy/ShiftEasyApplication.kt
@@ -0,0 +1,11 @@
+package de.thkoeln.gm.shifteasy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShiftEasyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShiftEasyApplication>(*args)
+}
---
+++ src/test/kotlin/de/thkoeln/gm/shifteasy/ShiftEasyApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.thkoeln.gm.shifteasy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShiftEasyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
