fcf82a4bfb6ac63683ae4175ea4c03a98dd159c0
rschlegeTH <148049069+rschlegeTH@users.noreply.github.com>
2024-01-20 17:15:14 +0100

    Employees
    Jetzt mit 3 Standard-Employees
    Now with 3 Standard-Employees

--- src/main/kotlin/de/thkoeln/gm/shifteasy/employee/EmployeeServiceImpl.kt
+++ src/main/kotlin/de/thkoeln/gm/shifteasy/employee/EmployeeServiceImpl.kt
@@ -25,6 +25,23 @@ class EmployeeServiceImpl (private val employeeRepository: EmployeeRepository) :
     }
 
     init {
-        
+        var employee1 = Employee()
+        var employee2 = Employee()
+        var employee3 = Employee()
+        employee1.lohn = 4000
+        employee1.stunden = 40
+        employee1.name = "Max Musterman"
+        employee1.job = "Javascript Dev"
+        employeeRepository.save(employee1)
+        employee2.lohn = 4200
+        employee2.stunden = 40
+        employee2.name = "Hans Gerhard"
+        employee2.job = "Typescript Dev"
+        employeeRepository.save(employee2)
+        employee3.lohn = 2000
+        employee3.stunden = 20
+        employee3.name = "Julius Maier"
+        employee3.job = "GDscript Dev"
+        employeeRepository.save(employee3)
     }
 }
