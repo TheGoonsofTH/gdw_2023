1796d5fd4d729b0d60849c544bff32cb0e7c5277
Marc Cremer <marc@cremer.dev>
2024-01-21 14:10:09 +0100

    findintimeframe

--- src/main/kotlin/de/thkoeln/gm/shifteasy/projects/ProjectsServiceImpl.kt
+++ src/main/kotlin/de/thkoeln/gm/shifteasy/projects/ProjectsServiceImpl.kt
@@ -1,17 +1,18 @@
 package de.thkoeln.gm.shifteasy.projects
 
 import de.thkoeln.gm.shifteasy.employee.Employee
-import de.thkoeln.gm.shifteasy.employee.EmployeeRepository
 import de.thkoeln.gm.shifteasy.employee.EmployeeService
-import de.thkoeln.gm.shifteasy.employee.EmployeeServiceImpl
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.Instant
-import java.util.UUID
+import java.util.*
 
 
 @Service
-class ProjectsServiceImpl (private val projectsRepository: ProjectsRepository,private val employeeService: EmployeeService) : ProjectsService {
+class ProjectsServiceImpl(
+    private val projectsRepository: ProjectsRepository,
+    private val employeeService: EmployeeService
+) : ProjectsService {
     override fun findById(id: UUID): Projects? {
         return projectsRepository.findByIdOrNull(id)
     }
@@ -29,15 +30,17 @@ class ProjectsServiceImpl (private val projectsRepository: ProjectsRepository,pr
     }
 
     override fun findInTimeframe(start_date: Instant, end_date: Instant): List<Employee> {
-        val projects = projectsRepository.findByStart_dateGreaterThanEqualAndEnd_dateLessThanEqual(start_date, end_date)
-        val employees = projects.stream().flatMap { it.employee.stream() }.map { it.id }
+        val projects = projectsRepository
+            .findByStart_dateGreaterThanEqualAndEnd_dateLessThanEqual(start_date, end_date)
+        val employees = projects
+            .stream()
+            .flatMap { it.employee.stream() }
+            .map { it.id }
+            .toList()
+            .toSet()
         val allEmployee = employeeService.findAll()
-        return allEmployee.stream().filter { employees.inc}
+        return allEmployee.toSet().filterNot { employees.contains(it.id) }
     }
 
 }
 
-fun filterListAById(listA: List<Employee>, listB: List<Employee>): List<Employee> {
-    val idsToRemove = listB.map { it.id }.toSet()
-    return listA.filterNot { idsToRemove.contains(it.id) }
-}
