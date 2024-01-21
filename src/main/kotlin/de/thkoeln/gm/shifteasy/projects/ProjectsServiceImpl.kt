fa667367fd2b76f667881c4510418145ec60af13
Marc Cremer <marc@cremer.dev>
2024-01-21 14:06:33 +0100

    filter 1

--- src/main/kotlin/de/thkoeln/gm/shifteasy/projects/ProjectsServiceImpl.kt
+++ src/main/kotlin/de/thkoeln/gm/shifteasy/projects/ProjectsServiceImpl.kt
@@ -1,6 +1,9 @@
 package de.thkoeln.gm.shifteasy.projects
 
 import de.thkoeln.gm.shifteasy.employee.Employee
+import de.thkoeln.gm.shifteasy.employee.EmployeeRepository
+import de.thkoeln.gm.shifteasy.employee.EmployeeService
+import de.thkoeln.gm.shifteasy.employee.EmployeeServiceImpl
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.Instant
@@ -8,7 +11,7 @@ import java.util.UUID
 
 
 @Service
-class ProjectsServiceImpl (private val projectsRepository: ProjectsRepository) : ProjectsService {
+class ProjectsServiceImpl (private val projectsRepository: ProjectsRepository,private val employeeService: EmployeeService) : ProjectsService {
     override fun findById(id: UUID): Projects? {
         return projectsRepository.findByIdOrNull(id)
     }
@@ -27,6 +30,14 @@ class ProjectsServiceImpl (private val projectsRepository: ProjectsRepository) :
 
     override fun findInTimeframe(start_date: Instant, end_date: Instant): List<Employee> {
         val projects = projectsRepository.findByStart_dateGreaterThanEqualAndEnd_dateLessThanEqual(start_date, end_date)
+        val employees = projects.stream().flatMap { it.employee.stream() }.map { it.id }
+        val allEmployee = employeeService.findAll()
+        return allEmployee.stream().filter { employees.inc}
     }
 
+}
+
+fun filterListAById(listA: List<Employee>, listB: List<Employee>): List<Employee> {
+    val idsToRemove = listB.map { it.id }.toSet()
+    return listA.filterNot { idsToRemove.contains(it.id) }
 }
